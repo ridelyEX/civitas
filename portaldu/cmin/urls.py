b54b893e729f2cfdc54c94735bb8364039b8f120
ridelyEX <arturovalruiz@gmail.com>
2025-05-30 09:06:48 -0600

    donde se esta haciendo todo?

---
+++ portaldu/cmin/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+
+urlpatterns = [
+
+]
